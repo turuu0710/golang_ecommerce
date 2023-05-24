e8ccf8c0dd968b59d81e36c0cb5bd6bbd639d575
Turuu <turuu0710@gmail.com>
2023-05-25 07:46:00 +0800

    second

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/turuu/ecommerce/controllers"
 	"github.com/turuu/ecommerce/database"
+	"github.com/turuu/ecommerce/routes"
 )
 
 func main() {
@@ -13,8 +15,18 @@ func main() {
 	if port == "" {
 		port = "8000"
 	}
-	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.Userdata(database.Client, "Products"))
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.Userdata(database.Client, "Users"))
 
 	router := gin.New()
-	router.Use(gin.Logger)
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.User(middleware.Authentication())
+
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.instantbuy())
+
+	log.Fatal(router.Run(":" + port))
 }
